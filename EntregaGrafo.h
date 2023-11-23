456691865831db90972422032aef7e50360e1ba8
Daniel Sanchez Parra <dsanchezfe@alumnos.unex.es>
2023-11-23 18:50:55 +0100

    lectura de datos
    lectura de datos

--- EntregaGrafo.h
+++ EntregaGrafo.h
@@ -5,15 +5,15 @@ class Grafo {
  float MatAdyacencia[MAX][MAX]; //fila(i) = origen, columna(j) = destino
  float MatFloyd [MAX][MAX];
  int MatP [MAX][MAX];
- ……… //Añadir los atributos y métodos que consideres necesarios
+//Añadir los atributos y métodos que consideres necesarios
 
 public:
  Grafo();
-void insertarVertice(string cad1)
+ void insertarVertice(string cad1)
  void insertarArista(string cad1, string cad2, float coste); // hay coste, por lo tanto, el grafo es ponderado
  void MostrarMatrizAdy(int matriz [MAX][MAX]);// creo que no hace falta pasar matriz por parametro
  void Camino(int i, int j, int MatP);
  int buscarPosicion (string cad1);
  void Floyd();
- ……… //Añadir los métodos que consideres necesarios
+ //Añadir los métodos que consideres necesarios
 }; 
--- main.cpp
+++ main.cpp
@@ -1,13 +1,43 @@
 //
 // Created by dsanchezp25 on 23/11/23.
 //
+#include <stdio.h>
  #include <iostream>
  #include <vector>
  #include <string>
+ #include "EntregaGrafo.h"
+
+ const int MAX_CITIES = 20;
+ const int MAX_CITY_NAME = 15;
 
  int mian(){
+   // Lectura del número de ciudades
+    int n;
+    cin >> n;
 
+    // Validar el rango de ciudades
+    if (n < 2 || n > MAX_CITIES) {
+        cerr << "El número de ciudades debe estar entre 2 y " << MAX_CITIES << "." <<endl;
+        return 1; // Código de error
+    }
 
-    
+   // Lectura de los nombres de las ciudades
+    vector<string> cities(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> cities[i];
+    }
+
+    // Lectura del número de arcos
+    int m;
+    cin >> m;
+
+    // Lectura de los arcos y distancias (no se almacenan en este ejemplo)
+    for (int i = 0; i < m; ++i) {
+        string city1, city2;
+        int distance;
+
+        cin >> city1 >> city2 >> distance;
+    }
+
     return 0;
  }
