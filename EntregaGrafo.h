d07984298528562e93c493309483d7d4b5f94f16
GMegiasH <114956345+gmegiash@users.noreply.github.com>
2023-12-03 13:12:27 +0100

    Update EntregaGrafo.h

--- EntregaGrafo.h
+++ EntregaGrafo.h
@@ -31,54 +31,83 @@ class Grafo {
     //Añadir los atributos y métodos que consideres necesarios
 
 public:
+
+    /*PRE:
+    *POST: inicializa las variables
+    */
     //constructor de grafo
     Grafo();
-    void setTamanoVectorRuta(int caminos);
 
-    //inicializa el numero de prioridades del grafo
+    /*PRE: 0 < prio <= MAX
+     *POST: inicializa el numero de prioridades del grafo
+    */
     void setPrioridades(int prio);
-    //devuelveel numero de prioridades del grafo
+
+    /*PRE:
+     *POST: devuelve el numero de prioridades del grafo
+    */
     int getPrioridades();
 
-    //inserta la ciudad en el conjunto de vertices
+    /*PRE:
+     *POST: inserta la ciudad en el conjunto de vertices
+    */
     void insertarVertice(string cad1);
 
-    /*
-     * inserta la distancia entre ciudades (coste) en la matriz de adyacencia
-     * hay coste, por lo tanto, el grafo es ponderado
-     *
-     * crea la matriz de adyacencia a partir del conjunto de vertices
-     * y
+    /*PRE:
+     *POST: inserta la distancia entre ciudades (coste) en la matriz de adyacencia, ademas, crea la matriz de adyacencia a partir del conjunto de vertices
      */
     void insertarArista(string cad1, string cad2, float coste);
 
-    //busca la ciudad pasada por parametro en el conjunto de vertices
+    /*PRE:
+     *POST: busca la ciudad pasada por parametro en el conjunto de vertices
+    */
     int buscarPosicion (string cad1);
 
-    //crea la matriz de Floy a partir de la matriz de adyacencia
+    /*PRE: Matriz de adyacencia creada e inicializada
+     *POST: crea la matriz de Floyd a partir de la matriz de adyacencia
+    */
     void Floyd();
 
-    //
+    /*PRE: Matriz de Floyd creada e inicializada
+     *POST: muestra la matriz de Floyd (camino de menor coste)
+    */
     void Camino(int i, int j, int MatP[MAX][MAX]);
 
-    //muestra la matriz de adyacencia
+    /*PRE:Matriz de adyacencia creada e inicializada
+     *POST: muestra la matriz de adyacencia
+    */
     void MostrarMatrizAdy();
 
-    //muestra la matriz de Floyd
-    void mostrarMatrizFloid();
+    /*PRE: Matriz de Floyd creada e inicializada
+     *POST: muestra la matriz de Floyd (llamando al metodo "Camino")
+    */
+    void mostrarMatrizFloyd();
 
+    /*PRE: Matriz de prioridades creada e inicializada
+     *POST: muestra la matriz de prioridades
+    */
     void mostrarMatrizP();
 
-    //crea la matriz de prioridades
+    /*PRE: 
+     *POST: crea la matriz de prioridades
+    */
     void matrizPrioridad(string cad1, string cad2, int prioridad);
 
-    //camino minimo entre ciudades
+    /*PRE: 
+     *POST: muestra los caminos minimos entre ciudades
+    */
     void caminosMinimos();
 
-    //cantidad minima de carreteras que deben arreglarse por prioridad
+    void setTamanoVectorRuta(int caminos);
+
+    /*PRE: 
+     *POST: muestra la cantidad minima de carreteras que deben arreglarse por prioridad
+    */
     float kilometrosArreglados();
 
-    //camino minimo entre ciudades con carreteras arregladas
+    /*PRE: 
+     *POST: muestra los caminos minimos entre ciudades con carreteras arregladas
+    */
     void caminosArreglados();
     //Añadir los métodos que consideres necesarios
 };
