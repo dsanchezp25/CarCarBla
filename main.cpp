59ee958462c87f390759ddf85c9ebb500aedc472
Daniel Sanchez Parra <148079505+dsanchezp25@users.noreply.github.com>
2023-12-05 15:06:23 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -31,31 +31,43 @@ void ejecucionGrafo(Grafo& g){
 }
 
 int main(){
-    Grafo G = *new Grafo(); //creamos el grafo G
+
+	Grafo G = *new Grafo(); //creamos el grafo G
     Grafo Prioridad = *new Grafo(); //creamos el grafo de prioridades
+
     // Lectura del número de ciudades
     int n;
     cin >> n;
 
     // Validar el rango de ciudades
     if (n < 2 || n > MAX_CITIES) {
+
         cerr << "El número de ciudades debe estar entre 2 y " << MAX_CITIES << "." <<endl;
         return 1; // Código de error
+
     }
 
     // Lectura de los nombres de las ciudades
     vector<string> cities(n);
+
     for (int i = 0; i < n; ++i) {
-        string city;
+
+    	string city;
         cin >> city;
+
         //se comprueba que el nombre este dentro del rango de caracteres
         while (city.length() > MAX_CITY_NAME){
+
             cout << "Vuelve a introducir un nombre de como maximo" << MAX_CITY_NAME << endl;
             cin >> city;
+
         }
+
         cities[i] = city;
+
         //insertamos las ciudades en los vertices
         G.insertarVertice(cities[i]);
+
     }
 
     // Lectura del número de arcos
@@ -64,24 +76,24 @@ int main(){
 
     // Validar el numero de arcos
     if (d < 1 || d > (n*n)) {
+
         cerr << "El número de arcos debe estar entre 1 y " << n*n << "." <<endl;
         return 1; // Código de error
+
     }
 
     // Lectura de los arcos y distancias (no se almacenan en este ejemplo)
     for (int i = 0; i < d; ++i) {
-        /*string line;
-        getline(cin, line); //leemos la linea entera, city1 city2 distance
 
-        istringstream iss(line);*/
         string city1, city2;
         float distance;
 
         //extrameos los datos de la linea
         cin >> city1 >> city2 >> distance;
 
-//        insertamos los datos de distancias entre ciudades
+        //insertamos los datos de distancias entre ciudades
         G.insertarArista(city1, city2, distance);
+
     }
 
     //Lectura de preguntas de caminos minimos
@@ -89,20 +101,23 @@ int main(){
 	cin >> p;
 
 	if(p < 1 || p > (n*n)){
+
 		cerr << "El número de caminos minimos debe estar entre 1 y " << n*n << "." <<endl;
 		return 1; // Código de error
+
 	}
 
 	G.setPreguntas(p);
-    vector<ruta>rutas(p);
+
 	for(int i = 0; i < p; i++){
+
 		string origen, destino;
 		cin >> origen >> destino;
-		rutas[i].origen = origen;
-		rutas[i].destino= destino;
-		G.setRutaGrafo(rutas[i] ,i);
-	}
 
+		//Da valor a los atributo origen y destino de la ruta
+		G.setRutaGrafo(origen, destino,i);
+
+	}
 
 	//prioridades
 	string prio;
@@ -110,6 +125,7 @@ int main(){
 
 	//Lectura de la prioridad que da la Junta
 	for (int i = 0; i < d; ++i) {
+
 		string city1, city2;
 		int prioridad;
 
@@ -117,12 +133,13 @@ int main(){
 		cin >> city1 >> city2 >> prioridad;
 
 		//insertamos los datos de prioridad de la carreta entre las ciudades
-
 		G.matrizPrioridad(city1, city2, prioridad);
 
 		if(prioridad > G.getCantidadPrioridades())
+
 			G.setCantidadPrioridades(prioridad);
-	 }
+
+	}
 
     //ejecutamos los metodos de los grafos
     ejecucionGrafo(G);
